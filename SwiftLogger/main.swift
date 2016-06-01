0eeed404454ac40accdf5716e4e5d48ee5f3dd6a
Sergey Rump (SPHERE) <Sergey.Rump@motorolasolutions.com>
2016-06-01 18:28:53 +0300

    Initial Commit

---
+++ SwiftLogger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLogger
+//
+//  Created by Sergey Rump (SPHERE) on 01.06.2016.
+//  Copyright © 2016 SR3u. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
